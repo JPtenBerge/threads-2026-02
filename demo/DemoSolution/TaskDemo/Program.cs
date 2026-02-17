6e6cbdbcc55bdcc7ee35df38951e655821c43913
JP <jp@jp.nl>
2026-02-17 14:56:16 +0100

    Eerste deel tasks

---
+++ demo/DemoSolution/TaskDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// var t = new Task<int>(() =>
+// {
+// 	Thread.Sleep(2000);
+// 	Console.WriteLine($"in task {Thread.CurrentThread.ManagedThreadId}");
+// 	return 42;
+// });
+// t.Start();
+
+// var task = Task.Run(() =>
+// {
+// 	return 14;
+// });
+
+// new TaskFactory<int>().StartNew()
+
+// Task afwachten? await
+
+// Console.WriteLine($"Result1: {t.GetAwaiter().GetResult()}");
+// Console.WriteLine($"Result2: {t.Result}");
+// Console.WriteLine($"Result3: {t.Result}");
+// Console.WriteLine($"Result4: {t.Result}");
+// Console.WriteLine($"Result5: {t.Result}");
+
+// Thread.Sleep(1000);
+// Console.WriteLine($"in main {Thread.CurrentThread.ManagedThreadId}");
+
+
+// cancellationtoken
+
+
+var cts = new CancellationTokenSource();
+Console.WriteLine("meteen annuleren");
+cts.Cancel();
+ 
+Task.Run(() =>
+{
+	Console.WriteLine("starting task");
+	for (int i = 0; i < 50_000_000; i++)
+	{
+		cts.Token.ThrowIfCancellationRequested();
+		Console.WriteLine($"i: {i}");
+	}
+}, cts.Token);
+
+Console.WriteLine("even dutje doen");
+Thread.Sleep(2000);
+Console.WriteLine("canceling!");
+cts.Cancel();
+
+Console.ReadKey();
--- demo/DemoSolution/ThreadsDemo/Program.cs
+++ demo/DemoSolution/ThreadsDemo/Program.cs
@@ -38,6 +38,25 @@ threadje1.Join();
 threadje2.Join();
 Console.WriteLine($"Tellertje is nu {MoeilijkDoen.s_tellertje}");
 
+// 2^32 = 2.147.xxx.xxx
+
+ThreadPool.GetAvailableThreads(out var aantal, out var diff);
+Console.WriteLine($"aantal/diff: {aantal}/{diff}");
+
+
+ThreadPool.SetMinThreads(5000, 1000 );
+ThreadPool.QueueUserWorkItem(_ =>
+{
+	Thread.Sleep(1000);
+	Console.WriteLine($"hallo vanaf threadpool thread {Thread.CurrentThread.ManagedThreadId}");
+});
+Thread.Sleep(2500);
+Console.WriteLine($"hallo vanaf main thread met 5000 threads {Thread.CurrentThread.ManagedThreadId}");
+
+Console.ReadKey();
+
+
+
 class MoeilijkDoen
 {
 	public static object lockObj = new { };
